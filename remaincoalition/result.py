73fb48d316ad5085841db11c6fbe8ac24d120ce8
David Batley <git@dbatley.com>
2019-09-29 14:06:16 +0100

    More

--- remaincoalition/result.py
+++ remaincoalition/result.py
@@ -8,7 +8,20 @@ from .party import get_party
 class Result(NamedTuple):
     results: Dict[Party, float]
 
-    # def adjust_for_polling()
+    def adjust_for_polling(self, adjustments: Dict[Party, float]) -> Result:
+        results = self.results.copy()
+        for party, adjust in adjustments.items():
+            try:
+                results[party] += adjust
+                if results[party] < 0.0:
+                    del results[party]
+            except KeyError:
+                pass
+        sum_votes = results.values()
+        normalization = 1.0 / sum_votes
+        return Result({
+            party: votes * normalization
+            for party, votes in results.items()})
 
     def collect_others(self, threshold: float) -> Result:
         results = {
