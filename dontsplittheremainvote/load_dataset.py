5bd471ef53ef477552d4295d7430d34131a6eb0f
David Batley <git@dbatley.com>
2019-10-02 23:58:16 +0100

    Fix indentation

--- dontsplittheremainvote/load_dataset.py
+++ dontsplittheremainvote/load_dataset.py
@@ -18,10 +18,10 @@ def get_all_datasets():
         data_ge2015.DESCRIPTION: data_ge2015.get_data(),
         data_ge2017_adjust_avg.DESCRIPTION: data_ge2017_adjust_avg.get_data(),
         data_ge2015_adjust_avg.DESCRIPTION: data_ge2015_adjust_avg.get_data(),
-	data_adj_con.DESCRIPTION_2017: data_adj_con.get_data_2017(),
-	data_adj_lab.DESCRIPTION_2017: data_adj_lab.get_data_2017(),
-	data_adj_ld.DESCRIPTION_2017: data_adj_ld.get_data_2017(),
-	data_adj_bx.DESCRIPTION_2017: data_adj_bx.get_data_2017(),
+        data_adj_con.DESCRIPTION_2017: data_adj_con.get_data_2017(),
+        data_adj_lab.DESCRIPTION_2017: data_adj_lab.get_data_2017(),
+        data_adj_ld.DESCRIPTION_2017: data_adj_ld.get_data_2017(),
+        data_adj_bx.DESCRIPTION_2017: data_adj_bx.get_data_2017(),
     }
     return datasets
 
