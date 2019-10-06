1a39c997316bd9cd182954f0a341a67576c72513
David Batley <git@dbatley.com>
2019-10-06 20:42:43 +0100

    Make a constituency index

--- dontsplittheremainvote/generate_pages.py
+++ dontsplittheremainvote/generate_pages.py
@@ -8,6 +8,7 @@ def generate_all_constituencies():
     constituency_pages = datasets_by_constituency()
     for constituency_page in constituency_pages:
         generate_constituency(constituency_page)
+    generate_index(constituency_pages)
 
 def generate_constituency(constituency_page):
     html = JINJA_ENV.get_template('constituency.html').render(
@@ -17,4 +18,11 @@ def generate_constituency(constituency_page):
         outcomes=constituency_page.outcomes,
         advice=constituency_page.advice)
     with open('generated/constituency/{}.html'.format(constituency_page.constituency.slug), 'w') as f:
+        f.write(html)
+
+def generate_index(constituency_pages):
+    html = JINJA_ENV.get_template('constituency_index.html').render(
+        static="/static",
+        constituency_pages=constituency_pages)
+    with open('generated/constituency/index.html', 'w') as f:
         f.write(html)
