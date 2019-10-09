2a7d8884559892c3c8f8026eb618f0f285b04794
David Batley <git@dbatley.com>
2019-10-09 23:32:08 +0100

    Re-order scenario list

--- dontsplittheremainvote/load_dataset.py
+++ dontsplittheremainvote/load_dataset.py
@@ -19,15 +19,16 @@ def get_all_datasets():
     datasets = {
         data_ge2017.DESCRIPTION: data_ge2017.get_data(),
         data_ge2015.DESCRIPTION: data_ge2015.get_data(),
+        data_eu2019.DESCRIPTION: data_eu2019.get_data(),
+
         data_ge2017_adjust_avg.DESCRIPTION: data_ge2017_adjust_avg.get_data(),
         data_ge2015_adjust_avg.DESCRIPTION: data_ge2015_adjust_avg.get_data(),
+        data_adj_avg.DESCRIPTION_2019: data_adj_avg.get_data_2019(),
+
         data_adj_con.DESCRIPTION_2017: data_adj_con.get_data_2017(),
         data_adj_lab.DESCRIPTION_2017: data_adj_lab.get_data_2017(),
         data_adj_ld.DESCRIPTION_2017: data_adj_ld.get_data_2017(),
         data_adj_bx.DESCRIPTION_2017: data_adj_bx.get_data_2017(),
-
-        data_eu2019.DESCRIPTION: data_eu2019.get_data(),
-        data_adj_avg.DESCRIPTION_2019: data_adj_avg.get_data_2019(),
     }
     return datasets
 
