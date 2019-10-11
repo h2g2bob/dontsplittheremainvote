db0214532bda90a8b1b42ce578b9424f0ea450b1
David Batley <git@dbatley.com>
2019-10-12 00:20:14 +0100

    Add dataset listing page

--- dontsplittheremainvote/generate_pages.py
+++ dontsplittheremainvote/generate_pages.py
@@ -1,5 +1,8 @@
 from jinja2 import Environment, FileSystemLoader
+from typing import List
+from .dataset import Dataset
 from .load_dataset import datasets_by_constituency
+from .load_dataset import get_all_datasets
 from .constituency import all_constituencies
 
 JINJA_ENV = Environment(loader=FileSystemLoader('templates/'))
@@ -9,6 +12,9 @@ def generate_all_constituencies():
     for constituency_page in constituency_pages:
         generate_constituency(constituency_page)
     generate_index(constituency_pages)
+
+    datasets = get_all_datasets()
+    generate_datasets(datasets)
 
 def generate_constituency(constituency_page):
     html = JINJA_ENV.get_template(constituency_page.advice.template).render(
@@ -28,4 +34,11 @@ def generate_index(constituency_pages):
     with open('generated/index.html', 'w') as f:
         f.write(html)
     with open('generated/constituency/index.html', 'w') as f:
+        f.write(html)
+
+def generate_datasets(datasets: List[Dataset]):
+    html = JINJA_ENV.get_template('datasets.html').render(
+        static="/static",
+        datasets=datasets)
+    with open('generated/datasets.html', 'w') as f:
         f.write(html)
