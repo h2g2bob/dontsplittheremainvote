0a0e6fa82b4474af124a099ac7e18314ed27674c
David Batley <git@dbatley.com>
2019-10-12 01:08:38 +0100

    Start of code for postcode search

---
+++ generated/postcodes.php
@@ -0,0 +1,19 @@
+<?php
+
+$db = new SQLite3('postcodes.sqlite3', SQLITE3_OPEN_READONLY);
+
+$PC = preg_replace("/[^A-Z0-9]/", "", strtoupper($_POST['postcode']));
+
+$stm = $db->prepare('SELECT slug FROM postcodes WHERE postcode = :postcode');
+$stm->bindValue(':postcode', $PC, SQLITE3_TEXT);
+
+$res = $stm->execute();
+
+$row = $res->fetchArray(SQLITE3_NUM);
+if ($row) {
+    header("location: /constituency/${row[0]}");
+} else {
+    header("location: /");
+}
+
+?>
