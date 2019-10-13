fda8d42263ab38ca00e0e37a4c453dfd868641db
David Batley <git@dbatley.com>
2019-10-13 15:20:31 +0100

    Add social media metadata

--- dontsplittheremainvote/generate_pages.py
+++ dontsplittheremainvote/generate_pages.py
@@ -8,6 +8,10 @@ from .generate_postcode_sqlite import make_sqlite
 
 JINJA_ENV = Environment(loader=FileSystemLoader('templates/'))
 
+BASE_URL = 'https://dontsplittheremainvote.com'
+STATIC = "/static"
+IMAGE_LOGO_735_238 = BASE_URL + STATIC + '/banner/banner_735_285.png'
+
 def generate_all_constituencies():
     make_sqlite()
 
@@ -20,19 +24,24 @@ def generate_all_constituencies():
     generate_datasets(datasets)
 
 def generate_constituency(constituency_page):
+    url_path = '/constituency/{}.html'.format(constituency_page.constituency.slug)
     html = JINJA_ENV.get_template(constituency_page.advice.template).render(
-        static="/static",
+        static=STATIC,
+        this_url=BASE_URL + url_path,
+        image_735_385=IMAGE_LOGO_735_238,
         constituency=constituency_page.constituency,
         datasets=constituency_page.datasets,
         outcomes=constituency_page.outcomes,
         advice=constituency_page.advice,
         **constituency_page.advice.advice_kwargs)
-    with open('generated/constituency/{}.html'.format(constituency_page.constituency.slug), 'w') as f:
+    with open('generated' + url_path, 'w') as f:
         f.write(html)
 
 def generate_index(constituency_pages):
     html = JINJA_ENV.get_template('constituency_index.html').render(
-        static="/static",
+        static=STATIC,
+        this_url=BASE_URL + '/',
+        image_735_385=IMAGE_LOGO_735_238,
         constituency_pages=constituency_pages)
     with open('generated/index.html', 'w') as f:
         f.write(html)
@@ -41,7 +50,9 @@ def generate_index(constituency_pages):
 
 def generate_datasets(datasets: List[Dataset]):
     html = JINJA_ENV.get_template('datasets.html').render(
-        static="/static",
+        static=STATIC,
+        this_url=BASE_URL + '/datasets.html',
+        image_735_385=IMAGE_LOGO_735_238,
         datasets=datasets)
     with open('generated/datasets.html', 'w') as f:
         f.write(html)
