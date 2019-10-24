a5df3a0b0bd91eb768509d64690af6c68e33825e
David Batley <git@dbatley.com>
2019-10-25 00:35:51 +0100

    Update polling for more recent polling

--- dontsplittheremainvote/data_adj_avg.py
+++ dontsplittheremainvote/data_adj_avg.py
@@ -9,7 +9,8 @@ from .party import LD
 from .party import SNP
 from .party import UKIP
 
-RECENT_POLLING = """October polling:
+RECENT_POLLING = """\
+Early October polling:
 
                Con   Lab   LibDem   Brexit   Green   SNP
 ComRes          33    27       19       13       3     4
@@ -20,8 +21,19 @@ https://www.comresglobal.com/polls/the-telegraph-voting-intention-october-2019/
 https://www.opinium.co.uk/political-polling-3rd-october-2019/
 https://yougov.co.uk/topics/politics/articles-reports/2019/10/11/political-trackers-8-9-oct-update
 
+Late October polling:
+
                Con   Lab   LibDem   Brexit   Green   SNP
-October Avg     35    24       19       12       4     4
+ComRes          33    29       18       12       4     4
+Opinium         37    24       16       12       4     4
+YouGov          37    22       19       11       7     3
+
+https://www.comresglobal.com/polls/britain-elects-voting-intention-poll-october-2019/
+https://www.opinium.co.uk/political-polling-3rd-october-2019-2/
+https://yougov.co.uk/topics/politics/articles-reports/2019/10/22/political-trackers-20-21-oct-update
+
+               Con   Lab   LibDem   Brexit   Green   SNP
+October Avg     35    25       18       12       5     4
 """
 
 DOC_2015 = """Results of the 2015 General Election, adjusted for an average opinion poll
@@ -31,7 +43,7 @@ DOC_2015 = """Results of the 2015 General Election, adjusted for an average opin
 2015 Election   37    30        8       13       4     5
 
                Con   Lab   LibDem   Brexit   Green   SNP
-Change          -2    -6      +11       -1       0    -1
+Change          -2    -5      +10       -1       1    -1
 
 The difference numbers are added the the percentage each party got in the election,
 and the results normalized to add up to 100%.
@@ -41,10 +53,11 @@ and the results normalized to add up to 100%.
 def _get_data_2015():
     adjustments = {
         CON: -0.02,
-        LAB: -0.06,
-        LD: +0.11,
+        LAB: -0.05,
+        LD: +0.10,
         UKIP: -0.01,
         SNP: -0.01,
+        GREEN: +0.01,
     }
     return {
         constituency: result.adjust_for_polling(adjustments)
@@ -64,7 +77,7 @@ DOC_2017 = """Results of the 2017 General Election, adjusted for an average opin
 2017 Election   42    40        7        2       1     3
 
                Con   Lab   LibDem   Brexit   Green   SNP
-Change          -7   -16      +12      +10      +3    +1
+Change          -7   -15      +11      +10      +4    +1
 
 The difference numbers are added the the percentage each party got in the election,
 and the results normalized to add up to 100%.
@@ -74,10 +87,10 @@ and the results normalized to add up to 100%.
 def _get_data_2017():
     adjustments = {
         CON: -0.07,
-        LAB: -0.16,
-        LD: +0.12,
+        LAB: -0.15,
+        LD: +0.11,
         UKIP: +0.10,
-        GREEN: +0.03,
+        GREEN: +0.04,
         SNP: +0.01,
     }
     return {
@@ -98,7 +111,7 @@ DOC_2019 = """Results of the 2019 European Parliament Election, adjusted for an 
 2019 Election    9    14       20       31      12     4
 
                Con   Lab   LibDem   Brexit   Green   SNP
-Change         +26   +10       -1      -19      -6     0
+Change         +26   +11       -2      -19      -5     0
 
 The difference numbers are added the the percentage each party got in the election,
 and the results normalized to add up to 100%.
@@ -108,10 +121,10 @@ and the results normalized to add up to 100%.
 def _get_data_2019():
     adjustments = {
         CON: +0.26,
-        LAB: +0.10,
-        LD: -0.01,
+        LAB: +0.11,
+        LD: -0.02,
         UKIP: -0.19,
-        GREEN: -0.06,
+        GREEN: -0.05,
         SNP: +0.00,
     }
     return {
