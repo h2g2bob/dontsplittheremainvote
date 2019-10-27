c4156440ce4528472b81c2cf8f8b9629fd804b49
David Batley <git@dbatley.com>
2019-10-27 18:20:35 +0000

    Make a csv file to show where other sources disagree

--- dontsplittheremainvote/generate_pages.py
+++ dontsplittheremainvote/generate_pages.py
@@ -1,13 +1,17 @@
+import csv
 import json
 from jinja2 import Environment, FileSystemLoader
 from typing import Dict
 from typing import List
+from typing import Tuple
 from .dataset import Dataset
 from .load_dataset import datasets_by_constituency
 from .load_dataset import get_all_datasets
 from .constituency import all_constituencies
+from .constituency import Constituency
 from .constituency_page import ConstituencyPage
 from .generate_postcode_sqlite import make_sqlite
+from .party import Party
 
 JINJA_ENV = Environment(loader=FileSystemLoader('templates/'))
 
@@ -25,6 +29,7 @@ def generate_all_constituencies():
         generate_constituency(constituency_page, nearby_constituencies)
     generate_index(constituency_pages)
     generate_json(constituency_pages)
+    generate_other_sites_csv(constituency_pages)
 
     datasets = get_all_datasets()
     generate_datasets(datasets)
@@ -111,3 +116,48 @@ def generate_json(constituency_pages: List[ConstituencyPage]):
         'constituencies': constituencies_data}
     with open('generated/data.json', 'w') as f:
         f.write(json.dumps(data, indent=2, sort_keys=True))
+
+def _generate_other_sites_data(constituency_pages: List[ConstituencyPage]) -> Tuple[Tuple[str], Dict[Constituency, Dict[str, Party]]]:
+    """
+    suggestions_by_constituency = {
+        Constituency: { site_name : Party },
+    }
+    """
+    suggestions_by_constituency = {}
+    all_site_names = set()
+    for cpage in constituency_pages:
+        suggestions_here = {
+            sug.who_suggests: sug.party
+            for sug in cpage.other_site_suggestions}
+        if cpage.advice.we_recommend_party is not None:
+            suggestions_here['DontSplit main'] = cpage.advice.we_recommend_party
+        suggestions_by_constituency[cpage.constituency] = suggestions_here
+        all_site_names |= suggestions_here.keys()
+    return tuple(all_site_names), suggestions_by_constituency
+
+def generate_other_sites_csv(constituency_pages: List[ConstituencyPage]):
+    all_site_names, all_suggestions = _generate_other_sites_data(constituency_pages)
+    with open('generated/other_sites.csv', 'w') as f:
+        writer = csv.writer(f)
+
+        headers = ['constituency', 'ons_id']
+        for site_name in all_site_names:
+            headers.append(site_name)
+            headers.append(site_name + " alignment")
+        writer.writerow(headers)
+
+        for constituency, con_sugg in all_suggestions.items():
+            data = [constituency.slug, constituency.ons_id]
+            for site_name in all_site_names:
+                try:
+                    party = con_sugg[site_name]
+                    alignment = sum(
+                        +1 if site_sugg == party else -1
+                        for site_sugg in con_sugg.values())
+                    alignment -= 1  # you match your own recommendation!
+                except KeyError:
+                    party = None
+                    alignment = None
+                data.append(party.short if party is not None else None)
+                data.append(alignment)
+            writer.writerow(data)
