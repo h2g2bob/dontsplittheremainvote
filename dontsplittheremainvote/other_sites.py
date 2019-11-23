9a404cf190ca732a7de7673893e85fb718b847e2
David Batley <git@dbatley.com>
2019-11-23 15:24:29 +0000

    More places are important (more local recommendations for "campagin here"

--- dontsplittheremainvote/other_sites.py
+++ dontsplittheremainvote/other_sites.py
@@ -144,7 +144,7 @@ def _tacticalvote_co_uk():
                 party=party,
                 url='https://tacticalvote.co.uk/#{}'.format(recomend['Constituency'].replace(' ', '')),
                 they_say=recomend['Why'],
-                important=(int(recomend['Priority']) == 1))
+                important=(int(recomend['Priority']) <= 2))
             yield get_constitiuency(recomend['id']), suggestion
 
 def _tactical_dot_vote():
