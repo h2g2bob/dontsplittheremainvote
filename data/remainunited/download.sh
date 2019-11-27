c72e79074e797738bf18988f2581983134c83a0d
David Batley <git@dbatley.com>
2019-11-27 23:18:54 +0000

    Remain United's site is super slow, so avoid ssl handshakes and download in parallel

--- data/remainunited/download.sh
+++ data/remainunited/download.sh
@@ -4,12 +4,31 @@ set -o noclobber
 set -o pipefail
 set -o nounset
 
-cat generated/example_postcodes.csv | while read line; do
-  slug=$(echo "$line" | cut -d , -f 1 | tr -d '\r')
-  pcode=$(echo "$line" | cut -d , -f 2 | tr -d '\r')
-  while ! curl 'https://www.remainunited.org/' --data "form_type=postcode-search&hid_form=Postcode+Search+Form&action=search&f_search_text_postcode=${pcode}&submit=submit" >| "data/remainunited/response/${slug}.html" ; do
-    sleep 120;
+function download() {
+  grep="$1"
+  cat generated/example_postcodes.csv | grep -E "${grep}" | while read line; do
+    slug=$(echo "$line" | cut -d , -f 1 | tr -d '\r')
+    pcode=$(echo "$line" | cut -d , -f 2 | tr -d '\r')
+    echo "$slug"
+    curl 'https://www.remainunited.org/' \
+      --data "form_type=postcode-search&hid_form=Postcode+Search+Form&action=search&f_search_text_postcode=${pcode}&submit=submit" \
+      --silent \
+      --retry 10 --retry-delay 30 \
+      >| "data/remainunited/response/${slug}.html"
   done
+}
 
-# -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:71.0) Gecko/20100101 Firefox/71.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-GB,en;q=0.5' --compressed -H 'Content-Type: application/x-www-form-urlencoded' -H 'Origin: https://www.remainunited.org' -H 'Connection: keep-alive' -H 'Referer: https://www.remainunited.org/' -H 'Cookie: PHPSESSID=1jjurneq387ktevu7mm4qtm0u6; _fbp=fb.1.1573391464135.20395060' -H 'Upgrade-Insecure-Requests: 1' -H 'DNT: 1' -H 'TE: Trailers' 
-done
+# remain united is super slow, so
+# * re-use a connection instead of making a new ssl connection each time
+# * do lots in paralell
+download '^[ab]'  &
+download '^[cd]'  &
+download '^[e]'  &
+download '^[fghijklm]'  &
+download '^[n]'  &
+download '^[opqr]'  &
+download '^[s]'  &
+download '^[tuvwxyz]'  &
+download '^[w]'  &
+jobs
+wait $(jobs -p)
