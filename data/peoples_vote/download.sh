aaf42190ba0762ee9d0ec7c5556a445a6aa0904d
David Batley <git@dbatley.com>
2019-12-08 14:00:14 +0000

    Update sources, including a fix for peoples vote downloader

--- data/peoples_vote/download.sh
+++ data/peoples_vote/download.sh
@@ -8,7 +8,7 @@ cat generated/example_postcodes.csv | while read line; do
   slug=$(echo "$line" | cut -d , -f 1 | tr -d '\r')
   pcode=$(echo "$line" | cut -d , -f 2 | tr -d '\r')
   out="data/peoples_vote/response/${slug}.html"
-  if ! ( find "$out" -cmin -60 > /dev/null && grep -F '</html>' "$out" > /dev/null ); then
+  if ! ( ! [ -z $( find "$out" -cmin -60 ) ] > /dev/null && grep -F '</html>' "$out" > /dev/null ); then
     curl --location "https://tactical-vote.uk/results?postcode=${pcode}" >| "${out}"
   fi
 done
